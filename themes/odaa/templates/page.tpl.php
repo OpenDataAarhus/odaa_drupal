57d8850c73196d31a5c5a0316fd1da425150e622
Rasmus Frey <rasmus@artz.dk>
2013-04-04 14:06:47 +0200

    Adding made with aarhus logo to drupal site also

--- themes/odaa/templates/page.tpl.php
+++ themes/odaa/templates/page.tpl.php
@@ -146,8 +146,11 @@
 <footer class="site-footer">
   <div class="container">
     <?php print render($page['footer']); ?>
-    <a href="http://www.itaward.dk/default.asp?Id=320&cpc=1" class="hide-text award-smart-city-footer-logo">Nomineret Årets Smart City-pris 2013</a>
-    <div class="attribution">
+    <div class="attribution attribution-first">      
+      <a class="hide-text award-smart-city-footer-logo" href="http://www.itaward.dk/default.asp?Id=320&amp;cpc=1">Nomineret Årets Smart City-pris 2013</a>
+      <a class="hide-text made-with-aarhus-footer-logo" href="http://www.aarhus.dk/da/omkommunen/english.aspx">Made with Aarhus - Danish for progress</a>      
+    </div>
+    <div class="attribution attribution-second">
       <p><strong>Powered by</strong></p>
       <a href="http://ckan.org" class="hide-text ckan-footer-logo">CKAN</a>
       <a href="http://drupal.org" class="hide-text drupal-footer-logo">Drupal</a>
